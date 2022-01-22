c282c7ac2fe43411ac0c412d950f42d451e2f815
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-01-22 08:13:43 +0530

    Update __main__.py

--- SteelaQueenBot/__main__.py
+++ SteelaQueenBot/__main__.py
@@ -709,7 +709,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}","*Zeus Is Ready ⚡*", parse_mode=ParseMode.MARKDOWN) 
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}","*Steela Is alive ⚡*", parse_mode=ParseMode.MARKDOWN) 
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!",
@@ -752,7 +752,7 @@ def main():
             updater.bot.set_webhook(url=URL + TOKEN)
 
     else:
-        LOGGER.info("Zeus is now alive and functioning")
+        LOGGER.info("Steela is now alive and functioning")
         updater.start_polling(timeout=15, read_latency=4, clean=True)
 
     if len(argv) not in (1, 3, 4):
