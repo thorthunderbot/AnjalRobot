536145442da195404701adfcdabfa1f92fcba19b
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-01-26 10:05:06 +0530

    Update __main__.py

--- SteelaQueenBot/__main__.py
+++ SteelaQueenBot/__main__.py
@@ -121,8 +121,8 @@ Helpful commands:
 - /start: Starts me! You've probably already used this.
 - /help: Sends this message; I'll tell you more about myself!
 - /donate: Gives you info on how to support me and my creator.
-If you want to report bugs or have any questions on how to use me then feel free to reach out: @ZeusSupportChat.
-All commands can be used with the following: *(/),(!),(?),(.),(~)*[!](https://telegra.ph/file/648aca75adfb9533a2161.jpg)
+If you want to report bugs or have any questions on how to use me then feel free to reach out: @NobisukiSupport.
+All commands can be used with the following: *(/),(!),(?),(.),(~)*[!](https://telegra.ph/file/a989d18c815397f11f84b.jpg)
 List of all the Modules:
 """.format(
     dispatcher.bot.first_name,
