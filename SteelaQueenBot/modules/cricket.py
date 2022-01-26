716262e8534c8c4d8b934cf71ba841b30b3f7bd9
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-01-26 09:46:47 +0530

    Update cricket.py

--- SteelaQueenBot/modules/cricket.py
+++ SteelaQueenBot/modules/cricket.py
@@ -2,7 +2,7 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 from telethon import events
-from ZeusXRobot import telethn as tbot
+from SteelaQueenBot import telethn as tbot
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
