7d82344e98a6c3850ee95a9d5bbbecae3486c66b
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-01-26 10:25:27 +0530

    Update userinfo.py

--- SteelaQueenBot/modules/userinfo.py
+++ SteelaQueenBot/modules/userinfo.py
@@ -252,9 +252,9 @@ def info(update: Update, context: CallbackContext):
         return
 
     rep = message.reply_text(
-        "<code> Connecting to Zeus </code>", parse_mode=ParseMode.HTML)
+        "<code> Appraising... </code>", parse_mode=ParseMode.HTML)
 
-    text = (f"╒═══「<b> Zeus DB Userinfo:</b> 」\n"
+    text = (f"╒═══「<b> Appraisal results:</b> 」\n"
             f"ID: <code>{user.id}</code>\n"
             f"First Name: {html.escape(user.first_name)}")
     
