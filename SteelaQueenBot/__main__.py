2bee43f56e5e787c4120eea201eb65b33f854df3
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-02-12 06:04:59 +0530

    Update __main__.py

--- SteelaQueenBot/__main__.py
+++ SteelaQueenBot/__main__.py
@@ -710,7 +710,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}","*Zeus Is Ready ⚡*", parse_mode=ParseMode.MARKDOWN) 
+            dispatcher.bot.sendMessage(f"@NobisukiSupport","[Anjal is back to online](https://telegra.ph/file/434967e94c3dda08b34ac.jpg)", parse_mode=ParseMode.MARKDOWN) 
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!",
