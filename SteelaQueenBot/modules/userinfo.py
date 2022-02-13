67452cac556784d25fbb7eca791c90eddcd92d37
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-02-13 16:26:02 +0530

    Update userinfo.py

--- SteelaQueenBot/modules/userinfo.py
+++ SteelaQueenBot/modules/userinfo.py
@@ -463,7 +463,7 @@ def stats(update, context):
                         "\n*Bot statistics*:\n"
                         + "\n".join(mod.__stats__() for mod in STATS)
                     )
-                    + f"\n\n✦ [Support](https://t.me/{SUPPORT_CHAT}) | ✦ [Updates](https://t.me/PegasusUpdates)\n\n"
+                    + f"\n\n✦ [Support](https://t.me/NobisukiSupport) | ✦ [Updates](https://t.me/PegasusUpdates)\n\n"
                 )
                 + "╘══「 by [God](https://t.me/call_me_crazyboy) 」\n"
             ),
