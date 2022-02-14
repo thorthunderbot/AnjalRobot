1bc84603d730d953415b4af1e1ee33ec7692f461
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-02-14 19:53:40 +0530

    Update __main__.py

--- SteelaQueenBot/__main__.py
+++ SteelaQueenBot/__main__.py
@@ -79,7 +79,7 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-𝐇𝐞𝐲 𝐈'𝐦 [🦋⃟Anjal](https://telegra.ph/file/434967e94c3dda08b34ac.jpg) 
+𝐇𝐞𝐲 𝐈'𝐦 [✿્᭄͜͡Ꭺ𝗇ʝaℓ🍒](https://telegra.ph/file/434967e94c3dda08b34ac.jpg) 
 𝐈 𝐇𝐚𝐯𝐞 𝐋𝐨𝐭 𝐎𝐟 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐚𝐧𝐝 𝐈 𝐂𝐚𝐧 𝐄𝐚𝐬𝐢𝐥𝐲 𝐌𝐚𝐧𝐚𝐠𝐢𝐧𝐠 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬! 𝐇𝐢𝐭 /help.
 
 ✰ 𝐅𝐞𝐞𝐥 𝐅𝐫𝐞𝐞 𝐓𝐨 𝐀𝐝𝐝 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬 ✰ """
