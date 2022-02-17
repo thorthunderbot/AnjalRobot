573bf03f771797749abbac7b3629c1ad65069bcf
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-02-17 18:34:12 +0530

    Update google.py

--- SteelaQueenBot/modules/google.py
+++ SteelaQueenBot/modules/google.py
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "🎒Tools"
+__mod_name__ = "🎒TOOLS"
 
 __help__ = """
  ❍ `/ud` (text)*:* meaning of the word
