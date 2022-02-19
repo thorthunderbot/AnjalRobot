a238a65e6413953712b3175bcd60710d47d6278b
Senthilkumar0 <89346905+Senthilkumar0@users.noreply.github.com>
2022-02-19 21:34:16 +0530

    Update eval.py

--- SteelaQueenBot/modules/eval.py
+++ SteelaQueenBot/modules/eval.py
@@ -77,7 +77,7 @@ def do(func, bot, update):
     os.chdir(os.getcwd())
     with open(
             os.path.join(os.getcwd(),
-                         'ZeusXRobot/modules/helper_funcs/temp.txt'),
+                         'SteelaQueenBot/modules/helper_funcs/temp.txt'),
             'w') as temp:
         temp.write(body)
 
