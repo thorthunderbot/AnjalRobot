5c2a4361364da85469f37d379d2ad239996a310a
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-02-19 09:23:46 +0530

    Update style_text.py

--- SteelaQueenBot/modules/style_text.py
+++ SteelaQueenBot/modules/style_text.py
@@ -322,7 +322,7 @@ __help__ = """
  ❍ `/text4`*:* reply to the text.
  ❍ `/text5`*:* reply to the text.
 """
-__mod_name__ = "🎇STYLETEXT"
+__mod_name__ = "🎇Styletext"
 
 TEXT1_HANDLER = DisableAbleCommandHandler("text1", text1)
 TEXT2_HANDLER = DisableAbleCommandHandler("text2", text2)
