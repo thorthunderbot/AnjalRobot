5e2fba301aa0f3e0bf062c500b34b62cad8b9703
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-02-19 08:40:05 +0530

    Update disable.py

--- SteelaQueenBot/modules/disable.py
+++ SteelaQueenBot/modules/disable.py
@@ -344,7 +344,7 @@ if is_module_loaded(FILENAME):
     • `/listcmds`*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "🔒DISABLE"
+    __mod_name__ = "🔒Disable"
 
 else:
     DisableAbleCommandHandler = CommandHandler
