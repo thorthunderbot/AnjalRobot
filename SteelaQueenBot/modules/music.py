ba83ab0994088b6c8159c36b9eb0d5dfe8c625aa
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-02-19 20:47:57 +0530

    Update music.py

--- SteelaQueenBot/modules/music.py
+++ SteelaQueenBot/modules/music.py
@@ -6,12 +6,11 @@ import os
 import time
 import requests
 import wget
-import youtube
 from urllib.parse import urlparse
 from pyrogram import filters
 from pyrogram.types import Message
 from tswift import Song
-from yt_dlp import YoutubeDL
+from import YoutubeDL
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
 from SteelaQueenBot.utils.pluginhelper import get_text, progress
