5b44501656f9d13a37f9a1a8e02442a053ea101d
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-02-20 09:26:23 +0530

    Create repo.py

---
+++ SteelaQueenBot/modules/repo.py
@@ -0,0 +1,23 @@
+from import craz
+from telethon import events, Button
+from telegram.parsemode import ParseMode
+
+ANJAL_PIC = "https://telegra.ph/file/f1603e71bdc0b60082609.jpg"
+
+@craz.on(events.NewMessage(incoming=True, pattern="/repo"))
+async def repo(e):
+    k = f"**Hi** {e.sender.first_name} **Thank You For Using Here is My Current Repo **"
+    BUTTON = [
+        [
+            Button.url("Repo", "https://github.com/godofanjal/AnjalRobot"),
+            Button.url("Owner", "https://telegram.me/call_me_crazyboy"),
+        ]
+    ]
+    await cray.send_file(
+        e.chat_id,
+        file=(ANJAL_PIC),
+        caption=k,
+        parse_mode=ParseMode.HTML,
+        buttons=BUTTON,
+        reply_to=e,
+    )
