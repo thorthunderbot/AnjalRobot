8ad6a2b4c390a580b9d4677fe7038e6b6e07ba90
Senthilkumar0 <89346905+Senthilkumar0@users.noreply.github.com>
2022-03-03 18:17:54 +0530

    Update misc.py

--- SteelaQueenBot/modules/helper_funcs/misc.py
+++ SteelaQueenBot/modules/helper_funcs/misc.py
@@ -67,11 +67,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 100:
         pairs = pairs[modulo_page * 100:100 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Back to Home", callback_data="zeus_back"),
+                EqInlineKeyboardButton("Go Back", callback_data="zeus_back"),
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("⬅ Back to Home ➡", callback_data="zeus_back")]]
+        pairs += [[EqInlineKeyboardButton("⬅ Go Back ➡", callback_data="zeus_back")]]
 
     return pairs
 
